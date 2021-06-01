3442283b099d77a3ded4718a8744877224391ae2
bedirhn.turhn <bedirhanturhan@ogr.iu.edu.tr>
2021-06-01 19:38:22 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Bedirhan Turhan on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
