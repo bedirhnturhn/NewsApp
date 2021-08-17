0f7394ecdad066cd604724c75c27f90517030b6d
Bedirhan <bedirhn.turhn@gmail.com>
2021-08-17 20:49:38 +0300

    added news content page, categorize page and categorize result page
    changed news object for fetching news object 
    added 3 page, created all page programmatically

---
+++ NewsApp/Views/FeedPage/FeedPageExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  FeedPageExtensions.swift
+//  NewsApp
+//
+//  Created by Bedirhan Turhan on 8/13/21.
+//
+
+import Foundation
---
+++ NewsApp/Views/NewsContentPage.swift
@@ -0,0 +1,8 @@
+//
+//  NewsContentPage.swift
+//  NewsApp
+//
+//  Created by Bedirhan Turhan on 8/13/21.
+//
+
+import Foundation
